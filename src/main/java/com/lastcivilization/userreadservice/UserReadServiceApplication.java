d54095252cf5afb9a67b1db88760a2925861c9ed
mrrevo55 <kwolny31@gmail.com>
2022-09-10 18:09:04 +0200

    Initial commit

---
+++ src/main/java/com/lastcivilization/userreadservice/UserReadServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lastcivilization.userreadservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserReadServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserReadServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lastcivilization/userreadservice/UserReadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lastcivilization.userreadservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserReadServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
