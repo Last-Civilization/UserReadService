955a6604f53bcd3873e1509d25d07efbd6159bc7
mrrevo55 <kwolny31@gmail.com>
2022-09-10 20:00:46 +0200

    Changed UserDto.java to record

---
+++ src/main/java/com/lastcivilization/userreadservice/infrastructure/application/rest/UserController.java
@@ -0,0 +1,10 @@
+package com.lastcivilization.userreadservice.infrastructure.application.rest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+class UserController {
+
+}
